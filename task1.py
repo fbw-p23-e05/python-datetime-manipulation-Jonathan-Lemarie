2414a8036de0896ef305378cfe24712fdd60ef93
Jonathan-Lemarie <jonathan.maheux-lemarie@dci-student.org>
2023-09-14 15:49:23 +0200

    adding exercises with corrections

--- task1.py
+++ task1.py
@@ -2,8 +2,10 @@
 # -- Using the variable called current_datetime, subtract 15 days from the current time.
 
 from datetime import datetime as dt, timedelta
+# import datetime as dt
+# timedelta can be called straight from datetime. or dt. (depending on the import)
+# makes it easier this way.
 
-#
 current_datetime= dt.today()
 # current date formatted 
 time_1 = current_datetime.strftime('%d/%m/%Y')
@@ -17,4 +19,19 @@ print('Current date is:', time_1)
 print('new date is: ', new_date)
 
 
+# correction  i would need to have only 'import datetime as dt'' above.
 
+current_datetime = dt.datetime.now()
+
+days = dt.timedelta(days=15)
+past_date = current_datetime - days
+print(past_date.date())
+
+# to have the specific date as mention in the exercice :
+
+current_datetime = '8/07/2021'
+new_date = dt.datetime.strptime(current_datetime, '%d/%m/%Y')
+
+days = dt.timedelta(days=15)
+past_date = new_date- days
+print(past_date.date())
--- task3.py
+++ task3.py
@@ -1,16 +1,19 @@
 
 # -- Your task is to write a reminder message for a customer that is being sent out on 2020-01-01 to please pay in 25 days. Create a string that stores a message to a customer called Friedrich, print out the message to the terminal.
 
+import datetime as dt
 
-from datetime import datetime, timedelta
 
+current_date = dt.datetime(year=2020, month=1, day=1)
 
-current_date = datetime(year=2020, month=1, day=1)
-
-due_date = current_date + timedelta(days=25)
+due_date = current_date + dt.timedelta(days=25)
 
 customer_name = 'Friedrich'
 
 
-print('hello,', customer_name, 'your rent of 300 euros is due on', due_date.strftime('%d %B, %Y'))
+print('hello', customer_name, 'your rent of 300 euros is due on', due_date.strftime('%d %B, %Y'))
 
+# correction below, another way to do it.
+
+rent_text = due_date.strftime('%d %B, %Y')
+print(f'Hello Friedrich, your rent of 300 € is due on {rent_text}')
